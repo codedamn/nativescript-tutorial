14b653ef63ba86348092c7d671ce4418f9547410
Mehul Mohan <mehulmpt@gmail.com>
2019-01-20 16:59:32 +0530

    Tutorial 10 done

--- app/main-page.js
+++ app/main-page.js
@@ -1,14 +1,3 @@
-
-
-exports.myFunction = args => {
-	const page = args.object
-	page.bindingContext = {
-		message: "Hello World from JavaScript!!!"
-	}
-
-	setTimeout(() => {
-		page.bindingContext = {
-			message: "Something else now"
-		}
-	}, 2000)
+exports.buttonPressed = () => {
+	alert('Button pressed!')
 }
